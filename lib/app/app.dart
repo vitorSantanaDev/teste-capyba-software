88b7c034a165a3714d1f0c86f308a7f8a62193ca
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-30 18:57:33 -0300

    feat: :lipstick: added some styles changes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,26 @@
+//package
 import 'package:flutter/material.dart';
+
+//pages
 import 'package:teste_capyba/pages/login_page.dart';
-
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color.fromRGBO(0, 233, 99, 1),
+        brightness: Brightness.dark
+      ),
+      home: const Scaffold(
         body: LoginPage(),
       ),
     );
