54bd546db26a32cd878cea83f1eefcbf64949c20
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-30 21:13:06 -0300

    feat: :sparkles: created routes for user registration and login

---
+++ lib/routes/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const URL_SIGNUP =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA3OFkEUYAM0AHV5f5BIfD9YiiYmRS5UnM';
+  static const URL_SIGNIN =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA3OFkEUYAM0AHV5f5BIfD9YiiYmRS5UnM";
+}
